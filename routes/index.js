71ca54db451bd31765fe5fab2b71b8db270e81a2
Kaleb Kuo <klkuo.cx@gmail.com>
2020-11-26 19:02:45 +0800

    Add userController and signup page via bcrypt & body-parser

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
 module.exports = (app) => {
   // user interface
   app.get('/', (req, res) => res.redirect('/restaurants'))
@@ -8,4 +9,8 @@ module.exports = (app) => {
   // admin interface
   app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', adminController.getRestaurants)
+
+  // signup
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 }
