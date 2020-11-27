bbba6bd3c5df76579f45d1880c2d46b987700d28
Kaleb Kuo <klkuo.cx@gmail.com>
2020-11-27 11:14:19 +0800

    Add admin create restaurant feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,6 +6,22 @@ const adminController = {
     Restaurant.findAll({ raw: true }).then(restaurants => {
       res.render('admin/restaurants', { restaurants })
     })
+  },
+
+  createRestaurant: (req, res) => {
+    res.render('admin/create')
+  },
+
+  postRestaurant: (req, res) => {
+    const restaurant = req.body
+    if (!restaurant.name) {
+      req.flash('error_messages', "name field is required.")
+      return res.redirect('back')
+    }
+    Restaurant.create(restaurant).then(restaurant => {
+      req.flash('success_messages', `restaurant '${restaurant.name}' was created successfully!`)
+      res.redirect('/admin/restaurants')
+    })
   }
 }
 module.exports = adminController
--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,8 @@ module.exports = (app) => {
   // admin interface
   app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
+  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
+  app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
 
   // signup & signin & logout
   app.get('/signup', userController.signUpPage)
